ebd7db0d7ef77540029da117304ca7529be706c9
Ivan Gromakovsky <gie1994@gmail.com>
2013-06-07 23:20:00 +0400

    Small changes in rshell

--- rshell/rshell.c
+++ rshell/rshell.c
@@ -15,6 +15,25 @@ const char * const SERVICE = "8822";
 const int BACKLOG = 32;
 const size_t CAPACITY = 4096;
 
+void write_all(int fd, const char * buf, size_t count) {
+    size_t written = 0;
+    while (written < count) {
+        int write_res = write(fd, buf, count - written);
+        if (write_res == -1) {
+            perror("write");
+            _exit(EXIT_FAILURE);
+        }
+        written += write_res;
+    }
+}
+
+void my_close(int fd) {
+    if (close(fd) == -1) {
+        perror("close");
+        _exit(EXIT_FAILURE);
+    }
+}
+
 int main() {
     int pid = fork();
     if (pid) {
@@ -23,6 +42,11 @@ int main() {
         pid_t sid = setsid();
         if (sid == (pid_t) -1) {
             perror("setsid");
+            _exit(EXIT_FAILURE);
+        }
+        int log_fd = dup(STDOUT_FILENO);
+        if (log_fd == -1) {
+            perror("dup");
             _exit(EXIT_FAILURE);
         }
         struct addrinfo hints;
@@ -37,34 +61,25 @@ int main() {
 
         int s = getaddrinfo(NULL, SERVICE, &hints, &result);
         if (s) {
-            perror(gai_strerror(s));
+            fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
             _exit(EXIT_FAILURE);
         }
 
-        struct addrinfo * i;
-        int sockfd;
-        for (i = result; i != NULL; i = i->ai_next) {
-            sockfd = socket(i->ai_family, i->ai_socktype, i->ai_protocol);
-            if (sockfd == -1) {
-                continue;
-            }
-            int yes = 1;
-            if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))) {
-                continue;
-            }
-            if (bind(sockfd, i->ai_addr, i->ai_addrlen) == -1) {
-                perror("bind");
-                _exit(EXIT_FAILURE);
-                break;
-            }
-            close(sockfd);
+        int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+        if (sockfd == -1) {
+            perror("socket");
+            _exit(EXIT_FAILURE);
+        }
+        int yes = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes))) {
+            perror("setsockopt");
+            _exit(EXIT_FAILURE);
+        }
+        if (bind(sockfd, result->ai_addr, result->ai_addrlen) == -1) {
+            perror("bind");
+            _exit(EXIT_FAILURE);
         }
         freeaddrinfo(result);
-
-        if (i == NULL) {
-            perror("setsockopt or socket");
-            _exit(EXIT_FAILURE);
-        }
 
         if (listen(sockfd, BACKLOG)) {
             perror("listen");
@@ -80,16 +95,16 @@ int main() {
                 perror("accept");
                 _exit(EXIT_FAILURE);
             }
-            if (fork()) { //a
+            write_all(log_fd, "Connected\n", strlen("Connected\n"));
+            if (!fork()) { //a
                 int master, slave;
                 char buf[CAPACITY];
-                int fd = openpty(&master, &slave, buf, NULL, NULL);
-                if (fd == -1) {
-                    perror("opentty");
+                if (openpty(&master, &slave, buf, NULL, NULL) == -1) {
+                    perror("openpty");
                     _exit(EXIT_FAILURE);
                 }
                 if (fork()) {
-                    close(slave);
+                    my_close(slave);
                     char buf[CAPACITY]; //from master to cfd
                     size_t len = 0;
                     char buf2[CAPACITY]; //from cfd to master 
@@ -142,34 +157,38 @@ int main() {
                             memmove(buf2, buf2 + w, len2);
                         }
                     }
-                    close(master);
-                    close(cfd);
+                    my_close(master);
+                    my_close(cfd);
                     _exit(EXIT_FAILURE);
                 } else {
-                    close(master);
-                    close(cfd);
-                    setsid();
+                    my_close(master);
+                    my_close(cfd);
+                    my_close(log_fd);
+                    if (setsid() == (pid_t) -1) {
+                        perror("setsid");
+                        _exit(EXIT_FAILURE);
+                    }
                     
                     int pty_fd = open(buf, O_RDWR);
                     if (pty_fd == -1) {
                         perror("open");
                         _exit(EXIT_FAILURE);
                     }
-                    close(pty_fd);
+                    my_close(pty_fd);
 
                     dup2(slave, STDIN_FILENO);
                     dup2(slave, STDOUT_FILENO);
                     dup2(slave, STDERR_FILENO);
 
-                    close(sockfd);
+                    my_close(slave);
                     execlp("bash", "bash", NULL);
                     _exit(EXIT_FAILURE);
                 }
             } else {
-                close(cfd);
+                my_close(cfd);
             }
         }
-        close(sockfd);
+        my_close(sockfd);
     }
     _exit(EXIT_SUCCESS);
 }
