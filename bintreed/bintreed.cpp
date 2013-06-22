852504af9fdc332f91ad3fdf5042b25c3467ed46
Ivan Gromakovsky <gie1994@gmail.com>
2013-06-22 19:57:43 +0400

    Added working bintreed

--- bintreed/bintreed.cpp
+++ bintreed/bintreed.cpp
@@ -11,6 +11,7 @@
 #include <poll.h>
 #include <errno.h>
 #include <string>
+#include <iostream>
 
 const char * const SERVICE = "8822";
 const int BACKLOG = 32;
@@ -36,12 +37,21 @@ void write_all(int fd, const char * buf, size_t count) {
 size_t read_all(int fd, char * buf) {
     int r;
     size_t len = 0;
-    while ((r = read(fd, buf, CAPACITY - len) > 0)) {
-        len += r;
+    r = read(fd, buf, CAPACITY - len);
+    if (buf[r - 1] == '\n') {
+        buf[r - 1] = '\0';
     }
-    if (r == -1) {
-        perror("read");
-        _exit(EXIT_FAILURE);
+    return r;
+    while (true) {
+        r = read(fd, buf, CAPACITY - len);
+        if (r == -1) {
+            perror("read");
+            _exit(EXIT_FAILURE);
+        }
+        if (r == 0) {
+            break;
+        }
+        len += r;
     }
     return len;
 }
@@ -54,54 +64,95 @@ void my_close(int fd) {
 }
 
 struct node {
-    node * left, * right, * parent;
+    node * left, * right;
     std::string value;
 
-    node() : left(nullptr), right(nullptr), parent(nullptr) {
+    node() : left(nullptr), right(nullptr) {
     }
 
     ~node() {
         delete left;
         delete right;
-        delete this;
     }
 };
 
 node * parse_tree(std::string s) {
     node * result = new node();
     if (s[0] != '<') {
-        return result;
-    }
-    size_t t = s.find('>');
-    if (t == std::string::npos) {
-        delete result;
         return nullptr;
     }
-    result->left = parse_tree(std::string(s.begin() + 1, s.begin() + t));
-    s.erase(0, t + 1);
-    t = s.find('<');
-    if (t == std::string::npos) {
-        return result;
+    std::string l;
+    size_t i = 1;
+    size_t k = 1;
+    while (i < s.length() && k != 0) {
+        if (s[i] == '<') {
+            ++k;
+        }
+        if (s[i] == '>') {
+            --k;
+        }
+        if (k == 0) {
+            result->left = parse_tree(l);
+            ++i;
+            break;
+        }
+        l += s[i];
+        ++i;
     }
-    result->value = s.substr(0, t);
-    s.erase(0, t);
-    result->right = parse_tree(s);
+    if (i == s.length()) {
+        return nullptr;
+    }
+
+    std::string value;
+    while (i < s.length() && s[i] != '<') {
+        value += s[i];
+        ++i;
+    }
+    if (i == s.length()) {
+        return nullptr;
+    }
+    result->value = value;
+
+    std::string r;
+    k = 1;
+    ++i;
+    while (i < s.length() && k != 0) {
+        if (s[i] == '<') {
+            ++k;
+        }
+        if (s[i] == '>') {
+            --k;
+        }
+        if (k == 0) {
+            result->right = parse_tree(r);
+            break;
+        }
+        r += s[i];
+        ++i;
+    }
+    if (i == s.length() && s.back() != ')') {
+        return nullptr;
+    }
+
     return result;
 }
 
 std::string tree_to_string(node * tree) {
-    std::string result;
+    std::string result = "<";
     if (tree->left == nullptr) {
-        result += "<()>";
+        result += "()";
     } else {
         result += tree_to_string(tree->left);
     }
+    result += ">";
     result += tree->value;
+    result += "<";
     if (tree->right == nullptr) {
-        result += "<()>";
+        result += "()";
     } else {
         result += tree_to_string(tree->right);
     }
+    result += ">";
     return result;
 }
 
@@ -131,9 +182,34 @@ node * move(node * root, std::string s) {
     return root;
 }
 
-/*int add(std::string s) {
-    return 0;
-}*/
+node * move_to_parent(node * root, std::string s) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+    node * parent = nullptr;
+    for (auto it = s.begin(); *it != 'h'; ++it) {
+        if (*it == 'l') {
+            if (root->left == nullptr) {
+                return nullptr;
+            } else {
+                parent = root;
+                root = root->left;
+            }
+            continue;
+        }
+        if (*it == 'r') {
+            if (root->right == nullptr) {
+                return nullptr;
+            } else {
+                parent = root;
+                root = root->right;
+            }
+            continue;
+        }
+        return nullptr;
+    }
+    return parent;
+}
 
 int main() {
     int pid = fork();
@@ -184,6 +260,9 @@ int main() {
         }
 
         node * root = new node();
+/*        root->value = "X";
+        root->right = new node();
+        root->right->value = "Y";*/
 
         pollfd fds[MAX_CLIENTS + 1];
         fds[0].fd = sockfd;
@@ -193,6 +272,7 @@ int main() {
         const short ERRORS = POLLERR | POLLHUP | POLLNVAL;
 
         while (true) {
+//            write_all(STDOUT_FILENO, "Poll\n", 0);
             int poll_res = poll(fds, nfds, POLL_TIMEOUT);
             if (poll_res == -1) {
                 perror("poll");
@@ -208,19 +288,59 @@ int main() {
 
                 if (fds[i].revents & POLLIN) {
                     char buf[CAPACITY];
-                    buf[CAPACITY - 1] = '\0';
                     size_t r = read_all(fds[i].fd, buf);
                     if (r > CAPACITY - 2) {
                         write_all(STDERR_FILENO, "Not enough CAPACITY", 0);
                         _exit(EXIT_FAILURE);
                     }
+                    buf[r] = '\0';
                     switch (buf[0]) {
                         case 'a': {
                             const char * command = "add";
                             buf[strlen(command)] = '\0';
                             if (!strcmp(buf, command)) {
+                                std::string s(buf + strlen(command) + 1);
+                                size_t t = s.find(' ');
+                                if (t == std::string::npos) {
+                                    write_all(fds[i].fd, "Invalid arguments\n", 0);
+                                    break;
+                                }
+                                std::string where = s.substr(0, t);
+                                if (where.back() != 'h') {
+                                    write_all(fds[i].fd, "Invalid arguments\n", 0);
+                                    break;
+                                }
+                                node * p = move_to_parent(root, where);
+                                s.erase(0, t + 1);
+                                node * tree = parse_tree(s);
+                                if (tree == nullptr) {
+                                    write_all(fds[i].fd, "Can't parse tree\n", 0);
+                                    break;
+                                }
+                                if (where == "h") {
+                                    root = tree;
+                                    write_all(fds[i].fd, "Added\n", 0);
+                                    break;
+                                }
+                                if (p == nullptr) {
+                                    write_all(fds[i].fd, "This node doesn't exist\n", 0);
+                                    delete tree;
+                                    break;
+                                }
+                                if (where[where.length() - 2] == 'l') {
+                                    p->left = parse_tree(s);
+                                    write_all(fds[i].fd, "Added\n", 0);
+                                    break;
+                                }
+                                if (where[where.length() - 2] == 'r') {
+                                    p->right = parse_tree(s);
+                                    write_all(fds[i].fd, "Added\n", 0);
+                                    break;
+                                }
+                                delete tree;
+                                write_all(fds[i].fd, "Invalid arguments\n", 0);
                             } else {
-                                write_all(fds[i].fd, "Invalid command", 0);
+                                write_all(fds[i].fd, "Invalid command\n", 0);
                             }
                             break;
                         }
@@ -228,28 +348,66 @@ int main() {
                             const char * command = "print";
                             buf[strlen(command)] = '\0';
                             if (!strcmp(buf, command)) {
+                                std::string s(buf + strlen(command) + 1);
                                 node * tree = move(root, buf + strlen(command) + 1);
                                 if (tree == nullptr) {
-                                    write_all(fds[i].fd, "Error occured", 0);
+                                    write_all(fds[i].fd, "This node doesn't exist", 0);
                                 } else {
                                     write_all(fds[i].fd, tree_to_string(tree).c_str(), 0);
                                 }
                             } else {
                                 write_all(fds[i].fd, "Invalid command", 0);
                             }
+                            write_all(fds[i].fd, "\n", 1);
                             break;
                         }
                         case 'd': {
                             const char * command = "del";
                             buf[strlen(command)] = '\0';
                             if (!strcmp(buf, command)) {
+                                std::string s(buf + strlen(command) + 1);
+                                if (s.back() != 'h') {
+                                    write_all(fds[i].fd, "Invalid arguments\n", 0);
+                                    break;
+                                }
+                                node * p = move_to_parent(root, s);
+                                if (s == "h") {
+                                    if (root->left) {
+                                        delete root->left;
+                                        root->left = nullptr;
+                                    }
+                                    if (root->right) {
+                                        delete root->right;
+                                        root->right = nullptr;
+                                    }
+                                    root->value = "";
+                                    write_all(fds[i].fd, "Deleted\n", 0);
+                                    break;
+                                }
+                                if (p == nullptr) {
+                                    write_all(fds[i].fd, "This node doesn't exist\n", 0);
+                                    break;
+                                }
+                                if (s[s.length() - 2] == 'l') {
+                                    delete p->left;
+                                    p->left = nullptr;
+                                    write_all(fds[i].fd, "Deleted\n", 0);
+                                    break;
+                                }
+                                if (s[s.length() - 2] == 'r') {
+                                    delete p->right;
+                                    p->right = nullptr;
+                                    write_all(fds[i].fd, "Deleted\n", 0);
+                                    break;
+                                }
+                                write_all(fds[i].fd, "Invalid arguments\n", 0);
                             } else {
-                                write_all(fds[i].fd, "Invalid command", 0);
+                                write_all(fds[i].fd, "Invalid command\n", 0);
                             }
                             break;
                         }
                         default: {
-                            write_all(fds[i].fd, "Invalid command", 0);
+                            write_all(fds[i].fd, "Invalid command\n", 0);
                         }
                     }
                 }
